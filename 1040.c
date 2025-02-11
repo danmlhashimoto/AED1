56b9cc8aa3ba7fcc66e4afeba2016a1f061460f2
danmlhashimoto <dan.hashimoto@unifesp.br>
2025-02-11 17:40:22 -0300

    Create 1040.c

---
+++ 1040.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    float N1, N2, N3, N4, media, exame, media_final;
+    
+    scanf("%f %f %f %f", &N1, &N2, &N3, &N4);
+    
+    media = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10;
+    printf("Media: %.1f\n", media);
+    
+    if (media >= 7.0) {
+        printf("Aluno aprovado.\n");
+    } else if (media < 5.0) {
+        printf("Aluno reprovado.\n");
+    } else {
+        printf("Aluno em exame.\n");
+        scanf("%f", &exame);
+        printf("Nota do exame: %.1f\n", exame);
+        
+        media_final = (media + exame) / 2;
+        
+        if (media_final >= 5.0) {
+            printf("Aluno aprovado.\n");
+        } else {
+            printf("Aluno reprovado.\n");
+        }
+        
+        printf("Media final: %.1f\n", media_final);
+    }
+    
+    return 0;
+}
